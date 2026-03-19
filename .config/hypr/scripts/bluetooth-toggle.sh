4abd937f72247c73487f76788503c6640d50c2e4
Cyber Falcon <thiolimor@gmail.com>
2026-03-19 00:21:25 -0300

    feat(hypr): Add icon on bluetooth toggle notification

--- .config/hypr/scripts/bluetooth-toggle.sh
+++ .config/hypr/scripts/bluetooth-toggle.sh
@@ -1,8 +1,8 @@
 STATE=`bluetoothctl show | grep Powered | awk '{print $2}'`
-if [[ $STATE == 'yes' ]]; then    
-    bluetoothctl power off    
-    notify-send "Bluetooth Off"
+if [[ $STATE == 'yes' ]]; then
+    bluetoothctl power off
+    notify-send "Bluetooth Off" -n blueman
 else
     bluetoothctl power on
-    notify-send "Bluetooth On"
+    notify-send "Bluetooth On" -n blueman
 fi
