049364ce95e075d7818ca4bfed6b1d15b1bc6595
Cyber Falcon <thiolimor@gmail.com>
2026-04-18 18:04:53 -0300

    feat(matugen): Use the main monitor for the matugen colors

--- .config/wpaperd/scripts/run-matugen.sh
+++ .config/wpaperd/scripts/run-matugen.sh
@@ -2,8 +2,14 @@
 
 display=$1
 wallpaper=$2
+default_display="eDP-2"
 
 echo "Display is : $display"
+if (( $display != $default_display )); then
+    echo "Skipped matugen on display '$display'"
+    exit 0
+fi
+
 echo "Wallpaper path is: $wallpaper"
 
 # Update Matugen
